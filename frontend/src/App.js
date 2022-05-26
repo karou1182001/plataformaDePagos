73e4989c0d9e930fd5991dbe7a35c73434523e42
María Zapata <54915677+karou1182001@users.noreply.github.com>
2022-05-26 02:31:11 -0500

    ft: Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 import CompShowUsers from './pages/showUsers';
 import CompCreateUser from './pages/createUser';
 import CompEditUser from './pages/editUser';
+import CompPagos from './pages/pagos';
 //importamos el router
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 
 
@@ -15,12 +17,13 @@ function App() {
 
 
 
-  
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
             <Route path='/' element={ <CompShowUsers />} />
+            <Route path='/pagos' element={ <CompPagos />} />
             <Route path='/create' element={ <CompCreateUser />} />
             <Route path='/edit/:id' element={ <CompEditUser />} />
         </Routes>
