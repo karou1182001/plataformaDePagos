6e1c1584916e37a3589b4b81eecc2f218950b018
María Zapata <54915677+karou1182001@users.noreply.github.com>
2022-06-06 17:10:43 -0500

    webhook

--- backend-webook/database/db.js
+++ backend-webook/database/db.js
@@ -5,6 +5,7 @@ import {Sequelize } from 'sequelize';
 /*-------------------MÉTODOS---------------------- */
 
 //Hace la conexión con la base de datos que actualmente está de manera local
-const db= new Sequelize("transaccion_webhook", "root", "112358", {host: "localhost", dialect: "mysql"});
+const db = new Sequelize('mysql://root:112358@mysqldb:3306/transaccion_webhook')
+//const db= new Sequelize("transaccion_webhook", "root", "112358", {host: "localhost", dialect: "mysql"});
 
 export default db
