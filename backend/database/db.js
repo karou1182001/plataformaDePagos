ce42af6fc631343ac2c5a4e6829f87dafc61a00a
María Zapata <54915677+karou1182001@users.noreply.github.com>
2022-06-06 10:59:54 -0500

    Ya se puede escalar la api de express

--- backend/database/db.js
+++ backend/database/db.js
@@ -8,5 +8,6 @@ import {Sequelize } from 'sequelize';
 //const db= new Sequelize("plat_de_pagos", "root", "112358", {host: "127.0.0.1", dialect: "mysql", port: "3307"})
 //const db= new Sequelize("plat_de_pagos", "root", "112358", {host: "mysqldb", dialect: "mysql"})
 const db = new Sequelize('mysql://root:112358@mysqldb:3306/plat_de_pagos')
+//const db = new Sequelize('mysql://root:112358@localhost:3306/plat_de_pagos')
 
 export default db
